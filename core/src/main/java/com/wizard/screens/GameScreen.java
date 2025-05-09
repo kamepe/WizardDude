2772e525f1b584baaf562bd58961ef58826d56e2
kamepe <mikelis@bwt.lv>
2025-05-09 14:43:24 +0200

    small stuff

--- core/src/main/java/com/wizard/screens/GameScreen.java
+++ core/src/main/java/com/wizard/screens/GameScreen.java
@@ -1,7 +1,6 @@
 package com.wizard.screens;
 
 import java.util.ArrayList;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
@@ -197,8 +196,7 @@ public class GameScreen extends ScreenAdapter {
     }
 
     public void update(float delta){
-        world.step(1/60f, 1, 1);  //CHECK this shit before implementing not sure about the velocity but prolly gonna use 30 frames
-        world.step(1/60f, 1, 1);
+        world.step(1/30f, 1, 1);
 
         camera.update();
         player.update(delta);
@@ -538,9 +536,6 @@ public class GameScreen extends ScreenAdapter {
                 font.draw(batch, promptText, screenWidth / 2 - 150, 50);
             }
         }
-
-
-
 
         batch.end();
     }
